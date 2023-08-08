7aafc07ecaa578d8187340415148ea2bd9ecd05c
summerhead <tiffany.cordova@proton.me>
2023-08-08 14:49:32 -0700

    Add spacing

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height + ')' + ' ');
+    document.write('<p>' + pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height + ')' + '</p>');
     
     if (pokemonList[i].height > 6) {
         document.write("- Wow, that's big!");
