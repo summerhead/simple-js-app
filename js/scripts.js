c0a49ee5886198b7dd3a8045ba3acc3564be672d
summerhead <tiffany.cordova@proton.me>
2023-08-10 13:21:21 -0700

    Exercise 1.5, Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,32 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     { name: 'Bulbasaur', height: 7, types: ['grass','poison'] },
     { name: 'Charmander', height: 6, types: ['fire'] },
-    { name: 'Squirtle', height: 5, types: ['water'] }
-];
+    { name: 'Squirtle', height: 5, types: ['water'] } ];
+
+    function add (item) {
+        if ((typeof(item) === 'object') && (Object.keys(item).includes("name") && Object.keys(item).includes("height") && Object.keys(item).includes("types"))) {
+            pokemonList.push(item);
+        } else {document.write('Error!')}
+    };
+     
+    function getAll () {
+        return pokemonList;
+    };
+
+    return {add,
+        getAll
+    };
+})();
 
 function pokemonDetails(pokemon) {
     document.write('<p>' + pokemon.name + ' ' + '(height:' + ' ' + pokemon.height + ')' + '</p>');
     if (pokemon.height > 6) {
         document.write("- Wow, that's big!");
     }
-}
+};
 
-pokemonList.forEach(pokemonDetails);
+
+pokemonRepository.getAll().forEach(pokemonDetails);
+
+
