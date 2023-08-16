b89359a0fe957c02c0a0f42173f761a0c1c144be
summerhead <tiffany.cordova@proton.me>
2023-08-16 15:36:35 -0700

    Exercise 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,43 @@
-let pokemonRepository = (function () {
+let pokemonRepository = (function() {
     let pokemonList = [
     { name: 'Bulbasaur', height: 7, types: ['grass','poison'] },
     { name: 'Charmander', height: 6, types: ['fire'] },
     { name: 'Squirtle', height: 5, types: ['water'] } ];
 
-    function add (item) {
+    function add(item) {
         if ((typeof(item) === 'object') && (Object.keys(item).includes("name") && Object.keys(item).includes("height") && Object.keys(item).includes("types"))) {
             pokemonList.push(item);
         } else {document.write('Error!')}
     };
      
-    function getAll () {
+    function getAll() {
         return pokemonList;
     };
 
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listItem.appendChild(button); // Append the button to the list item as its child
+        pokemonList.appendChild(listItem); // Append the list item to the unordered list as its child
+        eventListen(button,pokemon);
+    };
+
+    function eventListen(button,pokemon)  { // Add the event listener to the newly created button
+        button.addEventListener('click', function(pokemon) {
+            showDetails(pokemon);
+        });
+    };
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    };
+
     return {add,
-        getAll
+        getAll,
+        addListItem
     };
 })();
 
@@ -27,6 +49,8 @@ function pokemonDetails(pokemon) {
 };
 
 
-pokemonRepository.getAll().forEach(pokemonDetails);
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
 
 
