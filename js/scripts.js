eb6a20b285f7f0c35c2dadd80e832390157ce9ec
summerhead <tiffany.cordova@proton.me>
2023-11-20 10:17:09 -0800

    Exercise 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,68 @@ let pokemonRepository = (function() {
         button.classList.add("button-class");
         listItem.appendChild(button); // Append the button to the list item as its child
         pokemonList.appendChild(listItem); // Append the list item to the unordered list as its child
-        button.addEventListener('click', () => showDetails(pokemon));
+        button.addEventListener('click', () => showDetails(pokemon)); //showDetails(pokemon));
     };
 
-    function loadList() {
+    function showDetails(pokemon) { //execute loadDetails() which logs a Pokemon's details when a user clicks its button; show a modal with the Pokémon’s name, height, and image
+        pokemonRepository.loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+            showModal(pokemon);
+        });
+    }
+
+    function showModal(pokemon) { //execute loadDetails() which logs a Pokemon's details when a user clicks its button; show a modal with the Pokémon’s name, height, and image
+        let modalContainer = document.querySelector('#modal-container');
+
+        // Clear all existing modal content
+        modalContainer.innerHTML = '';
+        
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        
+        // Add the new modal content
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        
+        let nameElement = document.createElement('h1');
+        nameElement.innerText = pokemon.name;
+        let heightElement = document.createElement('p');
+        heightElement.innerText = "Height: " + pokemon.height;
+        let imgElement = document.createElement('img');
+        imgElement.classList.add('modal-img');
+        imgElement.setAttribute('src', pokemon.imageUrl);
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(nameElement);
+        modal.appendChild(heightElement);
+        modal.appendChild(imgElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+        
+        function hideModal() {
+            modalContainer.classList.remove('is-visible');
+        };
+        
+        window.addEventListener('keydown', (e) => {
+            let modalContainer = document.querySelector('#modal-container');
+            if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+                hideModal();
+            }
+        });
+
+        modalContainer.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
+        });
+    };
+
+    function loadList() { //fetch data from the API, then add each Pokémon in the fetched data to pokemonList with the add function
         return fetch(apiUrl).then(function (response) {
             return response.json();
         }).then(function (json) {
@@ -38,14 +96,13 @@ let pokemonRepository = (function() {
                     detailsUrl: item.url
                 };
                 add(pokemon);
-                console.log(pokemon);
             });
         }).catch(function (e) {
             console.error(e);
         })
     };
     
-    function loadDetails(item) {
+    function loadDetails(item) { //load the detailed data for a given Pokémon parameter
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
             return response.json();
@@ -59,20 +116,14 @@ let pokemonRepository = (function() {
         });
     };
 
-    function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
-            console.log(pokemon);
-        });
-    };
-
 
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        showDetails: showDetails,
         loadList: loadList,
         loadDetails: loadDetails,
-        showDetails: showDetails
     };
 })();
 
